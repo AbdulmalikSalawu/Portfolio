ca1fb1075438b02ca81d3d4c37a53055b337d8bf
Salawu Abdulmalik <salawuabdulmalik90@gmail.com>
2022-10-01 03:14:51 -0700

    Worked on the home page

---
+++ script.js
@@ -0,0 +1,40 @@
+function navlink(){
+   if(mylinks.style.display = "block"){
+        mylinks.style.display = "none"
+   }
+}
+function navlink2(){
+    if(mylinks.style.display = "block"){
+        mylinks.style.display = "none"
+   }
+}
+ function navlink3(){
+    if(mylinks.style.display = "block"){
+        mylinks.style.display = "none"
+   }
+}
+
+const navToggler=()=>{
+    mylinks.style.display = "block"
+    mylinks.innerHTML = `
+    <ul class="navbar-nav me-5 m-1 float-end m-auto chai">
+    <li class="nav-item me-5">
+        <a onclick = "navlink2()" href="#home" class="nav-link ">Home</a>
+      </li>
+      <li class="nav-item me-5">
+        <a onclick="navlink()" id="disp" href="#about" class="nav-link ">About</a>
+      </li>
+      <li class="nav-item me-5">
+        <a onclick="navlink3()" href ="#skills" class="nav-link">Skills</a>
+      </li>
+      <li class="nav-item me-5">
+        <a  href="" class="nav-link">Projects</a>
+      </li>
+      <li class="nav-item me-5">
+        <a class="nav-link">Contact</a>
+      </li>
+      <button class = " px-3">Resume</button>
+    </ul>
+    `
+    mylinks.style.background = "rgb(185, 132, 132)"
+}
